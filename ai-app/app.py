45ec481aea90a3f6517441867c4ba0da489b212a
Jani <kuhnojka@gmail.com>
2024-12-19 15:01:28 +0200

    ui update

--- ai-app/app.py
+++ ai-app/app.py
@@ -7,13 +7,15 @@ import numpy as np
 ### Requests predictions from ai-models API server
 ### Copyright Jani kuhno
 
-def generate_response(audio):
+def generate_response(audio, use_rag):
+    # For future, currently does nothing
+    print(f"RAG usage toggled: {use_rag}")
     with open(audio, 'rb') as f:
         response = requests.post("http://ai-models:8000/generate", files={"file": f})
     
     if response.status_code == 200:
         data = response.json()
-        return data["response"], None, None
+        return data["response"] #, None, None
     else:
         return f"{response.status_code}: There was an error generating the answer"
 
@@ -28,27 +30,34 @@ def read_response(answer):
     audio = np.array(data['audio'], dtype=np.float32)
     audio_tuple = (sampling_rate, audio)
 
-    return answer, audio_tuple
+    return audio_tuple # answer,
 
 
-with gr.Blocks() as app:
+#### UI #####
+#############
+
+
+theme = gr.themes.Soft()
+
+
+with gr.Blocks(theme="YTheme/Minecraft") as app:
+
     gr.HTML(
         f"""
-        <h1 style='text-align: center;'> Jarvis  🤵</h1>
+        <h1 style='text-align: center;'> LIZA  👩‍🔧</h1>
         <h3 style='text-align: center;'> The AI assistant from Temu </h3>
         """
     )
-    with gr.Group():
-        with gr.Row():
-            audio_out = gr.Audio(label="Spoken Answer", autoplay=True)
-            answer = gr.Textbox(label="Answer")
-            state = gr.State()
-        with gr.Row():
-            audio_in = gr.Audio(label="Speak your question", sources="microphone", type="filepath")
+    with gr.Column():
+        audio_in = gr.Audio(label="Speak your question", sources="microphone", type="filepath")
+        # For future use
+        use_rag = gr.Checkbox(label="Use retrieval memory")
+        rec_btn = gr.Button(value="Send to models")
+        answer = gr.Textbox(label="Answer as text")
+        audio_out = gr.Audio(label="Spoken Answer", autoplay=True)
 
-
-    audio_in.stop_recording(generate_response, audio_in, [state, answer, audio_out])\
-        .then(fn=read_response, inputs=state, outputs=[answer, audio_out])\
+    rec_btn.click(fn=generate_response, inputs=[audio_in, use_rag], outputs=answer).then(
+        fn=read_response, inputs=answer, outputs=audio_out)
 
 
 app.launch(share=False)
